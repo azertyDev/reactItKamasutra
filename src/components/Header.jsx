7b412939fb31a07e0aad3d708f0be5c860cbef16
azertyDev <khikmatovumid@mail.ru>
2020-03-16 20:00:54 +0500

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://www.vhv.rs/file/max/22/222442_png-logo.png"
+        className="App-logo"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.w3schools.com/howto/img_snow_wide.jpg"></img>
+      </div>
+      <div>ava + decsription</div>
+      <div>
+        My Posts
+        <div>New Post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
