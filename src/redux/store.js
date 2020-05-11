fc008d074b45fa948ad4329c618506e6beb8c36c
azertyDev <khikmatovumid@mail.ru>
2020-05-11 18:23:39 +0500

    Modify: store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,52 +2,54 @@ import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 
 let store = {
-	_state: {
-		profilePage: {
-			posts: [
-				{id: 1, message: "How are you?", likesCount: 23},
-				{id: 2, message: "My first post", likesCount: 12},
-				{id: 3, message: "Hello world!!!", likesCount: 42},
-			],
-			newPostText: "it-kamasutra.com",
-		},
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 12 },
+        { id: 2, message: "It's my first post", likesCount: 11 },
+        { id: 3, message: "Blabla", likesCount: 11 },
+        { id: 4, message: "Dada", likesCount: 11 },
+      ],
+      newPostText: "it-kamasutra.com",
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "Andrew" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Viktor" },
+        { id: 6, name: "Valera" },
+      ],
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How is your it-kamasutra?" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" },
+      ],
+      newMessageBody: "",
+    },
+  },
+  _callSubscriber() {
+    console.log("State changed");
+  },
 
-		dialogsPage: {
-			messages: [
-				{id: 1, message: "Hi"},
-				{id: 2, message: "How is your it-camasutra?"},
-				{id: 3, message: "Fine"},
-				{id: 4, message: "Thanks"},
-				{id: 5, message: "Yo"},
-				{id: 6, message: "Yo"},
-			],
-			dialogs: [
-				{name: "John", id: 1},
-				{name: "Tom", id: 2},
-				{name: "Bill", id: 3},
-				{name: "Adam", id: 4},
-				{name: "Steve", id: 5},
-				{name: "Emily", id: 6},
-			],
-			newMessageBody: "",
-		},
-	},
-	_callSubscriber() {
-		console.log("State changed!");
-	},
+  getState() {
+    debugger;
+    return this._state;
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer; // observer
+  },
 
-	getState() {
-		return this._state;
-	},
-	subscribe(observer) {
-		this._callSubscriber = observer;
-	},
-	dispatch(action) {
-		profileReducer(this._state.profilePage, action);
-		dialogsReducer(this._state.dialogsPage, action);
+  dispatch(action) {
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
 
-		this._callSubscriber(this._state);
-	},
+    this._callSubscriber(this._state);
+  },
 };
 
 export default store;
+window.store = store;
